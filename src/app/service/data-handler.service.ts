6ee26e07aa450091a4e49173a1e4d36efa992633
uspehgp <z995959z>
2021-02-06 15:29:41 +0300

    add Tasks views

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,16 +1,21 @@
 import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
+import {Task} from "../model/Task";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  getCategories(): Category[] {
-    return TestData.categories;
-  }
+    getCategories(): Category[] {
+        return TestData.categories;
+    }
+
+    getTasks(): Task[] {
+        return TestData.tasks
+    }
 }
