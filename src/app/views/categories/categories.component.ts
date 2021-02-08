c8aa483639aa04bee26109e76793684478347566
uspehgp <z995959z>
2021-02-08 12:35:27 +0300

    add all fields for tasks in view and dynamic active style for categories

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -11,6 +11,8 @@ export class CategoriesComponent implements OnInit {
 
     categories: Category[];
 
+    selectedCategory: Category;
+
     constructor(private dataHandler: DataHandlerService) {
     }
 
@@ -19,6 +21,7 @@ export class CategoriesComponent implements OnInit {
     }
 
     showTasksByCategory(category: Category) {
+        this.selectedCategory = category;
         this.dataHandler.fillTasksByCategory(category);
     }
 }
