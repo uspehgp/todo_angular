5f14186be1538e8cb7963b4282b467589cf65d9d
uspehgp <z995959z>
2021-02-18 19:50:15 +0300

    add sorting

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,18 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Task} from 'src/app/model/Task';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
     selector: 'app-tasks',
     templateUrl: './tasks.component.html',
     styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
     tasks: Task[];
     displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
     dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+    // ссылки на компоненты таблицы
+    @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+    @ViewChild(MatSort, {static: false}) private sort: MatSort;
 
     constructor(private dataHandler: DataHandlerService) {
     }
@@ -25,6 +31,14 @@ export class TasksComponent implements OnInit {
 
         this.refreshTable();
     }
+
+    // в этом методе уже все проинциализировано, поэтому можно присваивать объекты (иначе может быть ошибка undefined)
+    ngAfterViewInit(): void {
+
+        this.addTableObjects();
+
+    }
+
 
     toggleTaskCompleted(task: Task) {
         task.completed = !task.completed;
@@ -44,7 +58,43 @@ export class TasksComponent implements OnInit {
 
     }
 
+    // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
     private refreshTable() {
+
         this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+
+        this.addTableObjects();
+
+
+        // когда получаем новые данные..
+        // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+        // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
+        this.dataSource.sortingDataAccessor = (task, colName) => {
+
+            // по каким полям выполнять сортировку для каждого столбца
+            switch (colName) {
+                case 'priority': {
+                    return task.priority ? task.priority.id : null;
+                }
+                case 'category': {
+                    return task.category ? task.category.title : null;
+                }
+                case 'date': {
+                    return task.date ? task.date : null;
+                }
+
+                case 'title': {
+                    return task.title;
+                }
+            }
+        };
+
     }
+
+    private addTableObjects() {
+        this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+        this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
+    }
+
+
 }
